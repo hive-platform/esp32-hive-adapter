bd244d8209c43def129251a1a7427fdc963fae9b
Nicolas <nicolas.sansd@gmail.com>
2023-08-24 16:09:25 +0200

    adding headers functions

--- src/hive-adapter.cpp
+++ src/hive-adapter.cpp
@@ -3,4 +3,29 @@
 
 void HiveAdapter::_set_current_version() {
     std::cout << "Hello World" << std::endl;
+    HiveAdapter adapter("", "", "", CHECK_EVERY_X | CHECK_ON_STARTUP | CHECK_ON_CHANGE);
+    adapter.start();
+}
+
+char* HiveAdapter::get_version() {
+    if (this->version == NULL) {
+        return "NOT_DEFINED";
+    }
+    return this->version;
+}
+
+void HiveAdapter::set_secret(char* secret) {
+    this->secrets = secret;
+}
+
+HiveAdapter::HiveAdapter(char* secrets, char* hive_url, int flags) {
+    this->secrets = secrets;
+    this->hive_url = hive_url;
+    this->flags = flags;
+}
+
+void HiveAdapter::start() {
+    if (this->flags & CHECK_ON_STARTUP != 0) {
+        //TODO: check hive platform firmwares
+    }
 }
--- src/hive-adapter.h
+++ src/hive-adapter.h
@@ -1,9 +1,24 @@
 #ifndef _HIVE_ADAPTER
 #define _HIVE_ADAPTER
 
+#define NO_CHECK (0)
+#define CHECK_ON_STARTUP (1)
+#define CHECK_EVERY_X (2)
+#define CHECK_ON_CHANGE (4)
+
 class HiveAdapter {
+    HiveAdapter(char* secrets, char* hive_url = "", int flags = CHECK_ON_STARTUP);
     public:
+        void start();
+        void set_secret(char* secrets);
+        void set_listener_port(uint16_t port);
+        char* get_version();
     private:
+        char* secrets = NULL;
+        char* version = NULL;
+        char* hive_url = NULL;
+        
+        int flags = 0;
         void _set_current_version();
 };
 
